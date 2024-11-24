bf69292aef7a345365a6824aa6a3134404b1ae38
DimasAditya2 <esadimasadityaa@gmail.com>
2024-11-24 19:56:49 +0700

    fix: validation user

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  name: { type: String, require: true },
-  email: { type: String, require: true, unique: true },
-  password: { type: String, require: true },
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+    minlength: [3, "Name must be at least 3 characters long"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+    match: [/.+@.+\..+/, "Email must be valid"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: [6, "Password must be at least 6 characters long"],
+  },
+  imgUrl: {
+    type: String,
+    required: false,
+  },
 });
 
 module.exports = mongoose.model("User", UserSchema);
